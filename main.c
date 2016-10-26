feba295a4cba7376da45f19cdeef11a1e8515b68
Matthew Zhu <mzhu3@stuy.edu>
2016-10-26 11:51:49 -0400

    Started work

--- main.c
+++ main.c
@@ -7,8 +7,13 @@
 
 int createRandom() {
     int fd = open("dev/random");
-    
+    unsigned char *buffer[sizeof(int)];
+    int bytesRead = read(fd,buffer,4);
+    return rand(buffer);
 }
+
+int writeFile(char *intArray[]){
+  int fd = open("
 
 int main() {
     return 0;
